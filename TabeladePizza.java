41f801d6d1882c780075d6c0d7c3b0a872fc01f3
Ramon Rodrigues <ramonrodsouza@gmail.com>
2022-11-28 11:04:02 -0300

    Deixando TabelaDePizza mais Organizado

--- TabeladePizza.java
+++ TabeladePizza.java
@@ -7,78 +7,92 @@ public class TabeladePizza {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-				colocarTraco ();
+		colocarTraco();
 
-		String [] qualPizza = new  String[] { "Calabresa ","Mussarela", "Bacon", "Frango"};
-			mensagem ("Escolha qual o Sabor: " );
-		
-				iterarEExibirPosicoesDoVetorString(qualPizza);	
-				
-			Integer saborEscolhido = escolhaNumeroInteiro ("Voce escolheu o sabor: ", scanner );
-			Boolean escolhaValida = verificarPosicaoEscolhida(saborEscolhido,qualPizza);{
-			}
-				colocarTraco ();
-	
-			if(!escolhaValida) {
-				escolhaInvalida();
-			}
-			
-			String [] formaDePagamento = new String [] {"Cartão Credito", "Cartao Debito", "Dinheiro","Pix"};
-			mensagem("Escolha a forma de Pagamento: ");
-			
-				iterarEExibirPosicoesDoVetorString(formaDePagamento);
-				
-			Integer formaPagamentoEscolhida = escolhaNumeroInteiro("Sua forma de pagamento escolhida é: ", scanner);
-			Boolean Pagamento = verificarPosicaoEscolhida(formaPagamentoEscolhida,formaDePagamento);{
-			}
-			Boolean pagamentoEmPix = true;
-			
-			if(!Pagamento) {
-				escolhaInvalida();
-			}
-			
-			String pizzaEscolhida = qualPizza[saborEscolhido] ;
-			String pagamentoEscolhido = formaDePagamento[formaPagamentoEscolhida];
-				colocarTraco ();	
+		String pizzaEscolhida = pizza(scanner);
 
-			mensagem("Voce Escolheu Pizza de: " + pizzaEscolhida + ".");
-			mensagem( "Forma de Pagamento: " + pagamentoEscolhido + ".");
-			if (pagamentoEmPix) {
-				formaPagamentoEscolhida = 3;
-			mensagem("Pix: 21 972923210");
-			}
-			
-colocarTraco ();	
+		String pagamentoEscolhido = pagamento(scanner, pizzaEscolhida);
+
+		colocarTraco();
 
 		scanner.close();
 	}
-	static Integer escolhaNumeroInteiro (String texto, Scanner scanner) {
-		mensagemNaMesmaLinha (texto);
-		Integer numero = scanner.nextInt();
+
+	static String pizza(Scanner scanner) {
+		String[] qualPizza = new String[] { "Calabresa ", "Mussarela", "Bacon", "Frango" };
+		mensagem("Escolha qual o Sabor: ");
+
+		iterarEExibirPosicoesDoVetorString(qualPizza);
+		
+			Integer saborEscolhido = escolhaNumeroInteiro("Voce escolheu o sabor: ", scanner);
+			Boolean escolhaValida = verificarPosicaoEscolhida(saborEscolhido, qualPizza);
+		{
+		}
+		colocarTraco();
+
+		if (!escolhaValida) {
+			escolhaInvalida();
+		}
+		return qualPizza[saborEscolhido];
+	}
+
+	static String pagamento(Scanner scanner, String pizzaEscolhida) {
+		String[] formaDePagamento = new String[] { "Cartão Credito", "Cartao Debito", "Dinheiro", "Pix" };
+		mensagem("Escolha a forma de Pagamento: ");
+
+		iterarEExibirPosicoesDoVetorString(formaDePagamento);
+
+			Integer formaPagamentoEscolhida = escolhaNumeroInteiro("Sua forma de pagamento escolhida é: ", scanner);
+			Boolean pagamento = verificarPosicaoEscolhida(formaPagamentoEscolhida, formaDePagamento);
+			Boolean pagamentoEmPix = true;
+			String pagamentoEscolhido = formaDePagamento[formaPagamentoEscolhida];
+
+		if (!pagamento) {
+			escolhaInvalida();
+		}
+		colocarTraco();
+
+		mensagem("Voce Escolheu Pizza de: " + pizzaEscolhida + ".");
+		mensagem("Forma de Pagamento: " + pagamentoEscolhido + ".");
+
+		if (pagamentoEmPix = formaPagamentoEscolhida.equals(3)) {
+			mensagem("Chave Pix: 21 987654321");
+		}
+		return pagamentoEscolhido;
+	}
+
+	static Integer escolhaNumeroInteiro(String texto, Scanner scanner) {
+		mensagemNaMesmaLinha(texto);
+			Integer numero = scanner.nextInt();
 		return numero;
 	}
-	static Boolean verificarPosicaoEscolhida (Integer posicao, String [] vetor  ) {
-		Boolean valida = posicao >= 0 && posicao < vetor.length;
+
+	static Boolean verificarPosicaoEscolhida(Integer posicao, String[] vetor) {
+			Boolean valida = posicao >= 0 && posicao < vetor.length;
 		return valida;
 	}
-	static void colocarTraco () {
+
+	static void colocarTraco() {
 		System.out.println("-----------------------------------------");
-	}	
-	static void escolhaInvalida () {
+	}
+
+	static void escolhaInvalida() {
 		System.err.println("Opção Invalida!");
 		System.exit(1);
 	}
-	static void  iterarEExibirPosicoesDoVetorString(String[] vetor) {
-		for ( int i = 0 ; i < vetor.length; i++) {
+
+	static void iterarEExibirPosicoesDoVetorString(String[] vetor) {
+		for (int i = 0; i < vetor.length; i++) {
 			System.out.println("[ " + i + " ]" + vetor[i]);
 		}
 	}
+
 	static void mensagem(String texto) {
 		System.out.println(texto);
 	}
+
 	static void mensagemNaMesmaLinha(String texto1) {
 		System.out.print(texto1);
 	}
 
 }
-
